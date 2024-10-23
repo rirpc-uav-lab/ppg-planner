Fix coordinate calculation in GeometryDrawer and add visualization enhancements
- Corrected the calculation of top_left and bottom_right coordinates in the draw_square method of GeometryDrawer.
- Updated the NodeGraph class to initialize node_incidency_matrix with the provided parameter.
- Added visualization enhancements to the visualize_node_graph function, including saving the canvas as an image and handling nodes with string coordinates.
- Added a new visualize_node_graph function in quad_tree.py for local visualization.
- Updated the Quad class to flip the canvas vertically and save it as an image.
- Added comments to indicate unimplemented diagonal merging and handling of cases with three empty quads.
- Added additional conditions to handle nodes on the sides of the quad.
- Updated the TestClient class to include an additional polygon in the exclude_poly_list.

ppg_planner/point_in_poly_quad_tree.py
@@ -3,10 +3,8 @@ import time
 import rclpy
 import numpy as np
 import cv2 as cv
-from pympler import asizeof
 from rclpy.node import Node
 from copy import copy
-from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import PolygonStamped, Point32
 from ppg_planner_interfaces.msg import Poly
 from ppg_planner_interfaces.srv import PolyGrid
@@ -14,32 +12,42 @@ import ppg_planner.geometry as gm
 import ppg_planner.quad_tree as quad
 import ppg_planner.planners as planners
 
+from getpass import getuser
+from visualization_msgs.msg import Marker, MarkerArray
+from pympler import asizeof
 
 
 def visualize_node_graph(node_graph, canvas_size=1000, node_radius=2, line_thickness=1):
     # Create a black canvas
     canvas = np.zeros((canvas_size, canvas_size, 3), dtype=np.uint8)
 
-
     # Draw edges
     for i, connections in node_graph.node_incidency_matrix.items():
         node1 = node_graph.node_list[i]
-        center1 = (int(node1.center_point.x), int(node1.center_point.y))
+        center1 = (int(node1.x), int(node1.y))
         for j, distance in connections.items():
-            node2 = node_graph.node_list[j]
-            center2 = (int(node2.center_point.x), int(node2.center_point.y))
-            cv.line(canvas, center1, center2, (255, 0, 0), line_thickness)  # Draw edge as a white line
+            if j in node_graph.node_list:
+                node2 = node_graph.node_list[j]
+                center2 = (int(node2.x), int(node2.y))
+                cv.line(canvas, center1, center2, (255, 0, 0), line_thickness)  # Draw edge as a white line
+            else:
+                x, y = j.split(sep=",")
+                center2 = (int(float(x)), int(float(y)))
+                cv.line(canvas, center1, center2, (0, 0, 255), line_thickness)  # Draw edge as a white line
     # Draw nodes
-    for node in node_graph.node_list:
-        center = (int(node.center_point.x), int(node.center_point.y))
+    for uid, node in node_graph.node_list.items():
+        center = (int(node.x), int(node.y))
         cv.circle(canvas, center, node_radius, (0, 255, 0), -1)  # Draw node as a green circle
 
     # Flip the canvas vertically to match the coordinate system
-    canvas = cv.flip(canvas, 0)
+    canvas = cv.flip(canvas, -1)
+
+    cv.imwrite(f"/home/{getuser()}/Pictures/result_graph.png", canvas)    
 
     cv.imshow("Node Graph", canvas)
     cv.waitKey(0)
     cv.destroyAllWindows()
+    return canvas
 
 
 class PPGPlannerNode(Node):

ppg_planner/quad_tree.py
@@ -18,6 +18,39 @@ class Node:
         self.data = data
 
 
+def visualize_node_graph(node_graph, canvas_size=1000, node_radius=2, line_thickness=1):
+    # Create a black canvas
+    canvas = np.zeros((canvas_size, canvas_size, 3), dtype=np.uint8)
+
+    # Draw edges
+    for i, connections in node_graph.node_incidency_matrix.items():
+        node1 = node_graph.node_list[i]
+        center1 = (int(node1.x), int(node1.y))
+        for j, distance in connections.items():
+            if j in node_graph.node_list:
+                node2 = node_graph.node_list[j]
+                center2 = (int(node2.x), int(node2.y))
+                cv.line(canvas, center1, center2, (255, 0, 0), line_thickness)  # Draw edge as a white line
+            else:
+                x, y = j.split(sep=",")
+                center2 = (int(float(x)), int(float(y)))
+                cv.line(canvas, center1, center2, (0, 0, 255), line_thickness)  # Draw edge as a white line
+    # Draw nodes
+    for uid, node in node_graph.node_list.items():
+        center = (int(node.x), int(node.y))
+        cv.circle(canvas, center, node_radius, (0, 255, 0), -1)  # Draw node as a green circle
+
+    # Flip the canvas vertically to match the coordinate system
+    canvas = cv.flip(canvas, 1)
+
+    cv.imwrite(f"/home/{getuser()}/Pictures/result_graph.png", canvas)    
+
+    cv.imshow("Node Graph local", canvas)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    return canvas
+
+
 class Quad(SquareRegion):
     def __init__(self, center_point, side_size, max_size_downstep_level, current_size_downstep_level=0):
         super().__init__(center_point=center_point, side_size=side_size)
@@ -152,9 +185,13 @@ class Quad(SquareRegion):
             # print(frameinfo.filename, frameinfo.lineno)
             drawer.draw_square(self, canvas, color)
         
+
         if write:
             # canvas = cv.rotate(canvas, cv.ROTATE_180)
+            canvas = cv.flip(canvas, -1)
             cv.imwrite(f"/home/{getuser()}/Pictures/result.png", canvas)
+        return canvas
+        
     
     def include_zone(self, zone):
         if self.divided:
@@ -219,6 +256,10 @@ class Quad(SquareRegion):
             
             side_index = 2 ** counter
             diag_distance = min_side_size / math.sqrt(2)
+
+            # диагональные слияния не отработаны
+
+            # не отработаны случаи, когда три из квадов пустые
 
             if tl_nd is not None and tr_nd is not None:
                 tlcenter = self.top_left_quad.center_point
@@ -476,10 +517,10 @@ class Quad(SquareRegion):
                 incidency_mx.update(br_inmx)
                 node_dict.update(tr_nd)
                 node_dict.update(br_nd)
-                print(node_dict)
+                # print(node_dict)
             return node_dict, incidency_mx
         else:
-            if self.data == included:
+            if self.data == included or True:
                 side_size_tmp = copy(self.side_size)
                 counter = 0
                 while (side_size_tmp > min_side_size):
@@ -497,7 +538,8 @@ class Quad(SquareRegion):
                         node_dict[uid] = node
                         incidency_mx[uid] = {}
                         
-                        if (i, j) == (0,0):
+                        if (i, j) == (0,0): # bottom right corner
+                            # pass
                             ortho_neighbour1 = Point2d(x=(self.center_point.x + self.side_size / 2) + (0 * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (1 * min_side_size + 0.5 * min_side_size))
                             uid1 = f"{ortho_neighbour1.x},{ortho_neighbour1.y}"
                             ortho_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + (1 * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (0 * min_side_size + 0.5 * min_side_size))
@@ -507,7 +549,8 @@ class Quad(SquareRegion):
                             incidency_mx[uid][uid1] = min_side_size
                             incidency_mx[uid][uid2] = min_side_size
                             incidency_mx[uid][uid3] = diag_distance
-                        elif (i, j) == (side_index,0):
+                        # if (i, j) == (side_index,0):
+                        elif (i, j) == (side_index,0): # top right corner
                             ortho_neighbour1 = Point2d(x=(self.center_point.x + self.side_size / 2) + (side_index * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (1 * min_side_size + 0.5 * min_side_size))
                             uid1 = f"{ortho_neighbour1.x},{ortho_neighbour1.y}"
                             ortho_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + (side_index-1 * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (0 * min_side_size + 0.5 * min_side_size))
@@ -517,7 +560,7 @@ class Quad(SquareRegion):
                             incidency_mx[uid][uid1] = min_side_size
                             incidency_mx[uid][uid2] = min_side_size
                             incidency_mx[uid][uid3] = diag_distance
-                        elif (i, j) == (0,side_index):
+                        elif (i, j) == (0,side_index): # bottom left corner
                             ortho_neighbour1 = Point2d(x=(self.center_point.x + self.side_size / 2) + (1 * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (side_index * min_side_size + 0.5 * min_side_size))
                             uid1 = f"{ortho_neighbour1.x},{ortho_neighbour1.y}"
                             ortho_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + (0 * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (side_index-1 * min_side_size + 0.5 * min_side_size))
@@ -527,7 +570,7 @@ class Quad(SquareRegion):
                             incidency_mx[uid][uid1] = min_side_size
                             incidency_mx[uid][uid2] = min_side_size
                             incidency_mx[uid][uid3] = diag_distance
-                        elif (i, j) == (side_index,side_index):
+                        elif (i, j) == (side_index,side_index): # top left corner
                             ortho_neighbour1 = Point2d(x=(self.center_point.x + self.side_size / 2) + (side_index-1 * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (side_index * min_side_size + 0.5 * min_side_size))
                             uid1 = f"{ortho_neighbour1.x},{ortho_neighbour1.y}"
                             ortho_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + (side_index * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (side_index-1 * min_side_size + 0.5 * min_side_size))
@@ -537,14 +580,54 @@ class Quad(SquareRegion):
                             incidency_mx[uid][uid1] = min_side_size
                             incidency_mx[uid][uid2] = min_side_size
                             incidency_mx[uid][uid3] = diag_distance
+                        elif (j == 0): # right side
+                            ortho_neighbour1 = Point2d(x=(self.center_point.x + self.side_size / 2) + (i * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (1 * min_side_size + 0.5 * min_side_size))
+                            uid1 = f"{ortho_neighbour1.x},{ortho_neighbour1.y}"
+                            diag_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i+1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (1 * min_side_size + 0.5 * min_side_size))
+                            uid2 = f"{diag_neighbour2.x},{diag_neighbour2.y}"
+                            diag_neighbour3 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i-1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (1 * min_side_size + 0.5 * min_side_size))
+                            uid3 = f"{diag_neighbour3.x},{diag_neighbour3.y}"
+                            incidency_mx[uid][uid1] = min_side_size
+                            incidency_mx[uid][uid2] = diag_distance
+                            incidency_mx[uid][uid3] = diag_distance
+                        elif (i == side_index): # top side
+                            ortho_neighbour1 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((side_index - 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (j * min_side_size + 0.5 * min_side_size))
+                            uid1 = f"{ortho_neighbour1.x},{ortho_neighbour1.y}"
+                            diag_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((side_index - 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + 1) * min_side_size + 0.5 * min_side_size))
+                            uid2 = f"{diag_neighbour2.x},{diag_neighbour2.y}"
+                            diag_neighbour3 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((side_index - 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j - 1) * min_side_size + 0.5 * min_side_size))
+                            uid3 = f"{diag_neighbour3.x},{diag_neighbour3.y}"
+                            incidency_mx[uid][uid1] = min_side_size
+                            incidency_mx[uid][uid2] = diag_distance
+                            incidency_mx[uid][uid3] = diag_distance
+                        elif (j == side_index): # left side
+                            ortho_neighbour1 = Point2d(x=(self.center_point.x + self.side_size / 2) + (i * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((side_index - 1) * min_side_size + 0.5 * min_side_size))
+                            uid1 = f"{ortho_neighbour1.x},{ortho_neighbour1.y}"
+                            diag_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((side_index - 1) * min_side_size + 0.5 * min_side_size))
+                            uid2 = f"{diag_neighbour2.x},{diag_neighbour2.y}"
+                            diag_neighbour3 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i - 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((side_index - 1) * min_side_size + 0.5 * min_side_size))
+                            uid3 = f"{diag_neighbour3.x},{diag_neighbour3.y}"
+                            incidency_mx[uid][uid1] = min_side_size
+                            incidency_mx[uid][uid2] = diag_distance
+                            incidency_mx[uid][uid3] = diag_distance
+                        elif (i == 0): # bottom side
+                            ortho_neighbour1 = Point2d(x=(self.center_point.x + self.side_size / 2) + (1 * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + (j * min_side_size + 0.5 * min_side_size))
+                            uid1 = f"{ortho_neighbour1.x},{ortho_neighbour1.y}"
+                            diag_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + (1 * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + 1) * min_side_size + 0.5 * min_side_size))
+                            uid2 = f"{diag_neighbour2.x},{diag_neighbour2.y}"
+                            diag_neighbour3 = Point2d(x=(self.center_point.x + self.side_size / 2) + (1 * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j - 1) * min_side_size + 0.5 * min_side_size))
+                            uid3 = f"{diag_neighbour3.x},{diag_neighbour3.y}"
+                            incidency_mx[uid][uid1] = min_side_size
+                            incidency_mx[uid][uid2] = diag_distance
+                            incidency_mx[uid][uid3] = diag_distance
                         else:
                             ortho_neighbour1 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + 0) * min_side_size + 0.5 * min_side_size))
                             uid1 = f"{ortho_neighbour1.x},{ortho_neighbour1.y}"
-                            ortho_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + -1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + 0) * min_side_size + 0.5 * min_side_size))
+                            ortho_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i - 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + 0) * min_side_size + 0.5 * min_side_size))
                             uid2 = f"{ortho_neighbour2.x},{ortho_neighbour2.y}"
                             ortho_neighbour3 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + 0) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + 1) * min_side_size + 0.5 * min_side_size))
                             uid3 = f"{ortho_neighbour1.x},{ortho_neighbour1.y}"
-                            ortho_neighbour4 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + 0) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + -1) * min_side_size + 0.5 * min_side_size))
+                            ortho_neighbour4 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + 0) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j - 1) * min_side_size + 0.5 * min_side_size))
                             uid4 = f"{ortho_neighbour2.x},{ortho_neighbour2.y}"
                             incidency_mx[uid][uid1] = min_side_size
                             incidency_mx[uid][uid2] = min_side_size
@@ -553,11 +636,11 @@ class Quad(SquareRegion):
 
                             diag_neighbour1 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + 1) * min_side_size + 0.5 * min_side_size))
                             uid5 = f"{diag_neighbour1.x},{diag_neighbour1.y}"
-                            diag_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + -1) * min_side_size + 0.5 * min_side_size))
+                            diag_neighbour2 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j - 1) * min_side_size + 0.5 * min_side_size))
                             uid6 = f"{diag_neighbour2.x},{diag_neighbour2.y}"
-                            diag_neighbour3 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + -1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + -1) * min_side_size + 0.5 * min_side_size))
+                            diag_neighbour3 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i - 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j - 1) * min_side_size + 0.5 * min_side_size))
                             uid7 = f"{diag_neighbour1.x},{diag_neighbour1.y}"
-                            diag_neighbour4 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i + -1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + 1) * min_side_size + 0.5 * min_side_size))
+                            diag_neighbour4 = Point2d(x=(self.center_point.x + self.side_size / 2) + ((i - 1) * min_side_size + 0.5 * min_side_size), y=(self.center_point.y + self.side_size / 2) + ((j + 1) * min_side_size + 0.5 * min_side_size))
                             uid8 = f"{diag_neighbour2.x},{diag_neighbour2.y}"
                             incidency_mx[uid][uid5] = diag_distance
                             incidency_mx[uid][uid6] = diag_distance